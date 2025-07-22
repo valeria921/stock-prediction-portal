6fa36cba898952b8bc173aa56015555b18fc47f3
valeria921 <valeria.kruk921@gmail.com>
2025-07-22 13:09:28 +0200

    import order changed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import {useContext} from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import Button from './Button'
-import {Link, useNavigate} from 'react-router-dom'
 import { AuthContext } from '../AuthProvider'
 
 const Header = () => {
